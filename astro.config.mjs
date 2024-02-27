b8db1d5fc900a83bc6bd2e70b799a1454b2b9380
Marco Romeny <marco@mimecom.net>
2024-02-27 12:46:18 +0000

    Version 0.9.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
-
 import icon from "astro-icon";
+
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), alpinejs(), icon()]
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs(), icon(), pagefind()]
 });
