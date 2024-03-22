26a43fe90e71e09081d504dc078dafc062d49cef
Marco Romeny <marco@mimecom.net>
2024-03-22 11:57:09 -0400

    Handle rooms without images

--- src/content/config.ts
+++ src/content/config.ts
@@ -14,6 +14,7 @@ const artists = defineCollection({
         .array(
           z.object({
             title: z.string().optional(),
+            inventorynumber: z.string().optional(),
             info: z.string().optional(),
             credit: z.string().optional(),
             src: image(),
