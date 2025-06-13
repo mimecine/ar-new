d52b3914cc27f8fc4b16478fbe553dd56fc31903
Marco Romeny <marco@mimecom.net>
2025-06-13 15:14:33 +0200

    Tailwing upgrade tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
 //import { imageService } from "@unpic/astro/service";
@@ -12,7 +12,10 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://artistrooms.org',
-  integrations: [mdx(), sitemap(), tailwind(), alpinejs({entrypoint:'/src/alpine-entry.ts'}), icon(), pagefind(),],
+  integrations: [mdx(), sitemap(), alpinejs({entrypoint:'/src/alpine-entry.ts'}), icon(), pagefind(),],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   // image: {
   //   service: imageService({
   //     placeholder: "blurhash",
